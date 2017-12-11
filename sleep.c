a7d622883ed87ad4a8387208b5708f579e5a6732
Ryan Liptak <squeek502@hotmail.com>
2017-12-10 21:08:54 -0800

    Simplify/cleanup

--- sleep.c
+++ sleep.c
@@ -5,29 +5,25 @@
     #include <windows.h>
     #define EXPORT __declspec(dllexport)
 #else
-    #include <sys/time.h>
     #include <unistd.h>
     #define EXPORT
 #endif
 
-static int lc_sleep(lua_State *L)
+static int lua_sleep(lua_State *L)
 {
-    int ms = luaL_checkint(L, 1);
+    int ms = (int)luaL_checkinteger(L, 1);
     if (ms < 0)
         return luaL_error(L, "argument must be a positive integer");
 #ifdef _WIN32
     Sleep(ms);
 #else
-    struct timeval tv;
-    tv.tv_sec = (long)ms / 1000L;
-    tv.tv_usec = (long)ms % 1000L * 1000L;
-    select(0, NULL, NULL, NULL, &tv);
+    usleep(ms * 1000);
 #endif
     return 0;
 }
 
 EXPORT int luaopen_sleep(lua_State *L)
 {
-    lua_pushcfunction(L, lc_sleep);
+    lua_pushcfunction(L, lua_sleep);
     return 1;
 }
