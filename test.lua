862194dd8b436d64f8629e9a38cd28c7a27eed80
Ryan Liptak <squeek502@hotmail.com>
2017-12-19 18:54:40 -0800

    Fix test.lua on unix
    (clock doesn't update during sleep)

--- test.lua
+++ test.lua
@@ -1,12 +1,13 @@
 local sleep = require('sleep')
 
-local start = os.clock()
+local start = os.time()
 
 for i=1,10 do
-	sleep(1000)
-	io.write(i, " ")
+  sleep(1000)
+  io.write(i, " ")
+  io.flush()
 end
 io.write("\n")
 
-local duration = os.clock() - start
+local duration = os.time() - start
 assert(duration >= 9 and duration <= 11, "script ran for unexpected duration: " .. duration .. " seconds (expected ~10)")
