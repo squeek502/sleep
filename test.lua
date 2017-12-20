037f4d21f6a018fd63d3f9926136457c731c130e
Ryan Liptak <squeek502@hotmail.com>
2017-12-19 18:30:33 -0800

    Add an actual test to test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,12 @@
 local sleep = require('sleep')
 
+local start = os.clock()
+
 for i=1,10 do
 	sleep(1000)
-	print(i)
+	io.write(i, " ")
 end
+io.write("\n")
+
+local duration = os.clock() - start
+assert(duration >= 9 and duration <= 11, "script ran for unexpected duration: " .. duration .. " seconds (expected ~10)")
